34b5cb981c7a8e228853fe7f8a8e602862c5abf2
prishaanand <60487925+prishaanand@users.noreply.github.com>
2022-08-22 11:10:55 -0700

    Initial Commit

---
+++ War-Card-Game/War_Card_GameApp.swift
@@ -0,0 +1,17 @@
+//
+//  War_Card_GameApp.swift
+//  War-Card-Game
+//
+//  Created by Prisha Anand on 8/22/22.
+//
+
+import SwiftUI
+
+@main
+struct War_Card_GameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
