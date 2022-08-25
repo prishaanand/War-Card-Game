1809a2c4dfc2b4b6628fcbfa5c6c33241afe2a98
prishaanand <60487925+prishaanand@users.noreply.github.com>
2022-08-24 20:50:39 -0700

    completed the user interface and functionality

--- War-Card-Game/ContentView.swift
+++ War-Card-Game/ContentView.swift
@@ -8,9 +8,89 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    //properties
+    @State private var playerCard = "card5"
+    @State private var cpuCard = "card9"
+    @State private var playerScore = 0
+    @State private var cpuScore = 0
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        
+        ZStack{
+            Image("background")
+                .edgesIgnoringSafeArea([.all])
+            
+            VStack{
+                //logo
+                Spacer()
+                Image("logo")
+                    .padding([.all])
+                Spacer()
+                
+                //cards
+                HStack{
+                    Spacer()
+                    Image(playerCard)
+                    Spacer()
+                    Image(cpuCard)
+                    Spacer()
+                }
+
+                //deal button
+                Spacer()
+                Button (action: {
+                    
+                    // Generate a random number between 2-14
+                    let randPlayer = Int.random(in: 2...14)
+                    let randCpu = Int.random(in: 2...14)
+                    
+                    //update cards
+                    playerCard = "card" + String(randPlayer)
+                    cpuCard = "card" + String(randCpu)
+                    
+                    //update score
+                    if(randPlayer > randCpu){
+                        playerScore += 1
+                    }
+                    else if (randPlayer < randCpu){
+                        cpuScore += 1
+                    }
+                    //else, do nothing since equal
+                    
+                    
+                }, label: {
+                    Image("dealbutton")
+                })
+                Spacer()
+                
+                //score
+                HStack{
+                    Spacer()
+                    //player score
+                    VStack{
+                        Text("Player")
+                            .font(.headline)
+                            .padding(.bottom, 10.0)
+                        Text(String(playerScore))
+                            .font(.largeTitle)
+                    }.foregroundColor(Color.white)
+
+                    Spacer()
+                    //cpu score
+                    VStack{
+                        Text("CPU")
+                            .font(.headline)
+                            .padding(.bottom, 10.0)
+                        Text(String(cpuScore))
+                            .font(.largeTitle)
+                    }.foregroundColor(Color.white)
+                    Spacer()
+                }
+                
+                Spacer()
+            }
+        }
     }
 }
 
